4b08da3ac25c9a12ace14f1fd9052f799462dce1
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-06-11 19:31:35 +0300

    refactored calculator

--- src/Fee/Calculator.php
+++ src/Fee/Calculator.php
@@ -2,63 +2,12 @@
 
 namespace TransFeeCalc\Fee;
 
-use TransFeeCalc\Helpers\CardHelper;
-use TransFeeCalc\Helpers\ExchangeRateHelper;
 use TransFeeCalc\Transaction\DTO\Transaction;
 
 class Calculator
 {
-    private array $result = [];
-    public function __construct(
-        private readonly array $transactions
-    )
+    public function calculate(Transaction $transaction): float
     {
-    }
-
-    public function calculate(): static
-    {
-        foreach ($this->transactions as $transaction) {
-            if (!($transaction instanceof Transaction)) {
-                continue;
-            }
-            $amount = $transaction->getAmount();
-            $isEU = $this->isEU($transaction->getBin());
-            $rate = $this->getExchangeRateByCurrency($transaction->getCurrency());
-
-            $this->result[] = round(($amount / $rate) * (($isEU) ? 0.01 : 0.02), 2);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param string $binCode
-     * @return bool
-     */
-    public function isEU(string $binCode): bool
-    {
-        return CardHelper::isEuAssociated(CardHelper::getCountryCodeByBin($binCode));
-    }
-
-    /**
-     * @param string $currency
-     * @return int|mixed
-     */
-    public function getExchangeRateByCurrency(string $currency)
-    {
-        return ExchangeRateHelper::getExchangeRate($currency);
-    }
-
-    /**
-     * @return array
-     */
-    public function getResult(): array
-    {
-        return $this->result;
-    }
-
-    public function __toString(): string
-    {
-        return implode(PHP_EOL, $this->result);
+        return round(($transaction->getAmount() / $transaction->getRate()) * $transaction->getRatio(), 2);
     }
 }
