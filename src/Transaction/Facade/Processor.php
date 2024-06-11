65b6f4fca1e354ffcc5f0724a580038c6841944a
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-06-11 19:35:16 +0300

    implemented transaction Processor facade

---
+++ src/Transaction/Facade/Processor.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace TransFeeCalc\Transaction\Facade;
+
+use TransFeeCalc\Fee\Calculator;
+use TransFeeCalc\Fee\Ratio;
+use TransFeeCalc\Helpers\CardHelper;
+use TransFeeCalc\Helpers\ExchangeRateHelper;
+use TransFeeCalc\Transaction\DTO\Transaction;
+use TransFeeCalc\Transaction\Reader;
+
+class Processor
+{
+    public function __construct(
+        private Reader $reader
+    )
+    {
+    }
+
+    public function getResult(): \Generator
+    {
+        foreach ($this->reader->getFileData() as $row)
+        {
+            $dto = new Transaction($row['bin'], $row['amount'], $row['currency']);
+            $dto->setIsEU($this->isEU($dto->getBin()));
+            $dto->setRate($this->getExchangeRateByCurrency($dto->getCurrency()));
+            $dto->setRatio((new Ratio())->getRatio($dto));
+
+            yield (new Calculator())->calculate($dto);
+        }
+    }
+
+    /**
+     * @param string $binCode
+     * @return bool
+     */
+    public function isEU(string $binCode): bool
+    {
+        return CardHelper::isEuAssociated(CardHelper::getCountryCodeByBin($binCode));
+    }
+
+    /**
+     * @param string $currency
+     * @return int|mixed
+     * @throws \Exception
+     */
+    public function getExchangeRateByCurrency(string $currency): mixed
+    {
+        return ExchangeRateHelper::getExchangeRate($currency);
+    }
+}
