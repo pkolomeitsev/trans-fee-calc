f24d5ca31eeb438d04c9497f3c1c5d056195e70d
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-06-11 19:34:19 +0300

    extend Transaction DTO

--- src/Transaction/DTO/Transaction.php
+++ src/Transaction/DTO/Transaction.php
@@ -4,6 +4,9 @@ namespace TransFeeCalc\Transaction\DTO;
 
 class Transaction
 {
+    private bool $isEU;
+    private float $rate;
+    private float $ratio;
 
     /**
      * @param string $bin
@@ -46,6 +49,36 @@ class Transaction
     public function setCurrency(string $currency): void
     {
         $this->currency = $currency;
+    }
+
+    public function isEU(): bool
+    {
+        return $this->isEU;
+    }
+
+    public function setIsEU(bool $isEU): void
+    {
+        $this->isEU = $isEU;
+    }
+
+    public function getRate(): float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): void
+    {
+        $this->rate = $rate;
+    }
+
+    public function getRatio(): float
+    {
+        return $this->ratio;
+    }
+
+    public function setRatio(float $ratio): void
+    {
+        $this->ratio = $ratio;
     }
 
 }
