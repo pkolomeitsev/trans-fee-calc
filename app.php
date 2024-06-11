1d9988288edab081f69420fceb72d55494b7d2c5
pkolomeitsev <p.kolomeitsev@gmail.com>
2024-06-11 19:43:46 +0300

    main app.php refactoring

--- app.php
+++ app.php
@@ -2,18 +2,11 @@
 
 require_once 'vendor/autoload.php';
 
-use TransFeeCalc\Transaction\Aggregator;
 use TransFeeCalc\Transaction\Reader;
-use TransFeeCalc\Fee\Calculator as FeeCalculator;
+use TransFeeCalc\Transaction\Facade\Processor as FeeProcessor;
 
 $binFile = $argv[1] ?? 'input.txt';
 
-$data = (new Reader($binFile))
-    ->readData()
-    ->getData();
-
-$collection = (new Aggregator($data))
-    ->aggregate()
-    ->getCollection();
-
-echo (new FeeCalculator($collection))->calculate() . PHP_EOL;
+foreach ((new FeeProcessor(new Reader($binFile)))->getResult() as $fee){
+    echo $fee . PHP_EOL;
+}
